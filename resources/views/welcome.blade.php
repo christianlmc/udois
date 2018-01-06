91913a1ac1c5fc84e837edf48f19ad9158a63374
Christian Luis <christianlmc1@gmail.com>
2018-01-06 18:42:01 -0200

    View Home 100%

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 		<div class="container-fluid h-100">
 			<div class="row align-items-center h-100">
 				<div class="col-12 col-sm-5 offset-sm-7">
-					<div class="card bg-udois-blue text-white">
+					<div class="card bg-udois-blue text-white" style="opacity: .9;">
 						<div class="card-body text-right">
 							<h5 class="card-title ">{{ $pagina->frase_principal }}
 								<img class="rounded-circle float-right" width="80px" src="{{ $admin->foto_perfil ? Storage::url('profiles/' . $admin->foto_perfil) : asset('user.png')}}">
